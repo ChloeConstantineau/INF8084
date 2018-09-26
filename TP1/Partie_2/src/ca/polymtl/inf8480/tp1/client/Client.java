7e58f863f43a9de80334d33f7957de5306bb5c56
Chloé Constantineau <chloe.constantineau@polymtl.ca>
2018-09-26 14:21:43 -0400

    Set up directory for file system client and server side

--- TP1/Partie_2/src/ca/polymtl/inf8480/tp1/client/Client.java
+++ TP1/Partie_2/src/ca/polymtl/inf8480/tp1/client/Client.java
@@ -5,7 +5,9 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Arrays;
 
+import ca.polymtl.inf8480.tp1.shared.ConsoleOutput;
 import ca.polymtl.inf8480.tp1.shared.ServerInterface;
 
 public class Client {
@@ -18,6 +20,8 @@ public class Client {
 		String localHostname = "127.0.0.1";
 		functionName = "";
 		param = new String[2];
+		Arrays.fill(param, "");
+
 		if (args.length >= 1) {
 			functionName = args[0];
 		}
@@ -32,7 +36,8 @@ public class Client {
 		client.run();
 	}
 
-	private ServerInterface localServerStub;
+	private ServerInterface AuthServerStub;
+	private ServerInterface FileSystemStub;
 
 
 	public Client(String serverHostname) {
@@ -41,28 +46,28 @@ public class Client {
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
 		}
-		localServerStub = loadServerStub(serverHostname);
+		AuthServerStub = loadServerStub(serverHostname, "authServer");
+		FileSystemStub = loadServerStub(serverHostname, "fileSystemServer");
 	}
 
 	private void run() {
-		if(localServerStub != null) {
+		if(AuthServerStub != null && FileSystemStub != null) {
 			executeCall();
 		}
 	}
 
-	private ServerInterface loadServerStub(String hostname) {
+	private ServerInterface loadServerStub(String hostname, String registryName) {
 		ServerInterface stub = null;
 
 		try {
 			Registry registry = LocateRegistry.getRegistry(hostname);
-			stub = (ServerInterface) registry.lookup("server");
+			stub = (ServerInterface) registry.lookup(registryName);
 		} catch (NotBoundException e) {
-			System.out.println("Erreur: Le nom '" + e.getMessage()
-					+ "' n'est pas défini dans le registre.");
+			System.out.println(ConsoleOutput.REGISTRY_NOT_FOUND);
 		} catch (AccessException e) {
-			System.out.println("Erreur: " + e.getMessage());
+			System.out.println("Error: " + e.getMessage());
 		} catch (RemoteException e) {
-			System.out.println("Erreur: " + e.getMessage());
+			System.out.println("Error: " + e.getMessage());
 		}
 
 		return stub;
@@ -72,14 +77,47 @@ public class Client {
 
 		switch(functionName){
 			case "new": {
-				if(param[0] == )
+				if(param[0] == "" || param[1] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					newClient(param[0], param[1]);
 			}
-			case "verify":
+			case "verify":{
+				if(param[0] == "" || param[1] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					verifyClient(param[0], param[1]);
+			}
 			case "create":
-			case "list":
-			case "syncLocalDirectory":
+			{
+				if(param[0] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					create(param[0]);
+			}
+			case "list": list();
+			case "syncLocalDirectory": syncLocalDirectory();
 			case "get":
+				{
+				if(param[0] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					get(param[0]);
+			}
 			case "lock":
+				{
+				if(param[0] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					lock(param[0]);
+			}
+			case "push":
+			{
+				if(param[0] == "")
+					System.out.println(ConsoleOutput.INVALID_FUNCTION_CALL);
+				else
+					push(param[0]);
+			}
 		}
 	}
 
@@ -87,7 +125,31 @@ public class Client {
 
 	}
 
-	private void VerifyClient(String login, String password){
+	private void verifyClient(String login, String password){
+
+	}
+
+	private void create(String name){
+
+	}
+
+	private void list(){
+
+	}
+
+	private void syncLocalDirectory(){
+
+	}
+
+	private void get(String name){
+
+	}
+
+	private void lock(String name){
+
+	}
+
+	private void push(String name){
 
 	}
 }
